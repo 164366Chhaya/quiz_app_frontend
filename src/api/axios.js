2ca7ddb46f412ce3381e9919ea98acb637bb71f8
Shubham Kumar Saw <shubhamkumarsaw120@gmail.com>
2026-04-15 15:13:22 +0530

    fix env var syntax for CRA

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
+const API = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081/api',
   withCredentials: true
 });
